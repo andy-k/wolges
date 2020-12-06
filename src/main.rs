c7f913e59c6a9b41721b8b3b26e0d008669dd0af
Andy Kurnia <andykurn@gmail.com>
2020-12-06 15:27:01 +0800

    strider

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,32 @@ mod game_config;
 mod gdw;
 mod matrix;
 
+fn prt(z: matrix::Strider) {
+    for c in 0..z.len() {
+        println!("{} = {}", c, z.at(c))
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    prt(matrix::Strider {
+        base: 5,
+        step: 2,
+        len: 15,
+    });
+    // row 4. base=row*cols, len=cols, step=1
+    prt(matrix::Strider {
+        base: 4 * 15,
+        step: 1,
+        len: 15,
+    });
+    // col 4. base=col, len=rows, step=cols
+    prt(matrix::Strider {
+        base: 4,
+        step: 15,
+        len: 15,
+    });
+    prt(matrix::Dim { rows: 15, cols: 15 }.across(3));
+    prt(matrix::Dim { rows: 15, cols: 15 }.down(7));
     let gdw = gdw::from_bytes(&std::fs::read("csw19.gdw")?);
     println!("{}", gdw.0.len());
     let gc = &game_config::COMMON_ENGLISH_GAME_CONFIG;
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,63 +1,48 @@
 #[derive(Clone, Copy)]
+pub struct Strider {
+    pub base: i8,
+    pub step: i8,
+    pub len: i8,
+}
+
+impl Strider {
+    #[inline(always)]
+    pub fn len(&self) -> i8 {
+        self.len
+    }
+    #[inline(always)]
+    pub fn at(&self, idx: i8) -> usize {
+        ((self.base as isize) + (idx as isize) * (self.step as isize)) as usize
+    }
+}
+
+#[derive(Clone, Copy)]
 pub struct Dim {
     pub rows: i8,
     pub cols: i8,
 }
 
 impl Dim {
+    #[inline(always)]
+    pub fn across(&self, row: i8) -> Strider {
+        Strider {
+            base: row * self.cols,
+            step: 1,
+            len: self.cols,
+        }
+    }
+
+    #[inline(always)]
+    pub fn down(&self, col: i8) -> Strider {
+        Strider {
+            base: col,
+            step: self.cols,
+            len: self.rows,
+        }
+    }
+
+    #[inline(always)]
     pub fn at_row_col(&self, row: i8, col: i8) -> usize {
         (((row as isize) * (self.cols as isize)) + (col as isize)) as usize
-    }
-}
-
-#[derive(Clone, Copy)]
-pub struct Across {
-    dim: Dim,
-    row: i8,
-}
-
-#[derive(Clone, Copy)]
-pub struct Down {
-    dim: Dim,
-    col: i8,
-}
-
-pub trait Flippable {
-    fn lanes(&self) -> i8;
-    fn len(&self) -> i8;
-    fn at(&self, col: i8) -> usize;
-}
-
-impl Flippable for Across {
-    #[inline(always)]
-    fn lanes(&self) -> i8 {
-        self.dim.rows
-    }
-
-    #[inline(always)]
-    fn len(&self) -> i8 {
-        self.dim.cols
-    }
-
-    #[inline(always)]
-    fn at(&self, col: i8) -> usize {
-        self.dim.at_row_col(self.row, col)
-    }
-}
-
-impl Flippable for Down {
-    #[inline(always)]
-    fn lanes(&self) -> i8 {
-        self.dim.cols
-    }
-
-    #[inline(always)]
-    fn len(&self) -> i8 {
-        self.dim.rows
-    }
-
-    #[inline(always)]
-    fn at(&self, row: i8) -> usize {
-        self.dim.at_row_col(row, self.col)
     }
 }
