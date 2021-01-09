937c5c278c36724aa6490bf0a12209bcd065ed9e
Andy Kurnia <andykurn@gmail.com>
2021-01-10 03:44:32 +0800

    overtimes

--- src/main_auto.rs
+++ src/main_auto.rs
@@ -3,6 +3,66 @@
 use super::{display, error, game_config, game_state, klv, kwg, move_filter, move_picker, movegen};
 use rand::prelude::*;
 
+struct GameTimers {
+    instant: std::time::Instant,
+    clocks_ms: Box<[i64]>,
+    turn: i8, // -1 for nobody's
+}
+
+impl GameTimers {
+    fn new(num_players: u8) -> Self {
+        Self {
+            instant: std::time::Instant::now(),
+            clocks_ms: vec![0; num_players as usize].into_boxed_slice(),
+            turn: -1,
+        }
+    }
+
+    fn reset_to(&mut self, initial_ms: i64) {
+        self.clocks_ms.iter_mut().for_each(|m| *m = initial_ms);
+        self.turn = -1;
+        self.instant = std::time::Instant::now();
+    }
+
+    fn set_turn(&mut self, new_turn: i8) {
+        let new_instant = std::time::Instant::now();
+        if self.turn >= 0 && (self.turn as usize) < self.clocks_ms.len() {
+            self.clocks_ms[self.turn as usize] -= new_instant
+                .saturating_duration_since(self.instant)
+                .as_millis() as i64;
+        }
+        self.instant = new_instant;
+        self.turn = new_turn;
+    }
+}
+
+fn print_ms(mut ms: i64) {
+    if ms < 0 {
+        print!("-");
+        ms = -ms;
+    }
+    let just_ms = ms % 1000;
+    let sec = ms / 1000;
+    let just_sec = sec % 60;
+    let min = sec / 60;
+    print!("{:02}:{:02}.{:03}", min, just_sec, just_ms);
+}
+
+fn print_timers(timers: &GameTimers) {
+    print!("Timers: ");
+    for (i, &timer) in timers.clocks_ms.iter().enumerate() {
+        if i != 0 {
+            print!(", ")
+        }
+        if i as isize == timers.turn as isize {
+            print!("*")
+        }
+        print!("Player {}: ", i + 1);
+        print_ms(timer);
+    }
+    println!();
+}
+
 pub fn main() -> error::Returns<()> {
     let kwg = kwg::Kwg::from_bytes_alloc(&std::fs::read("csw19.kwg")?);
     let klv = klv::Klv::from_bytes_alloc(&std::fs::read("leaves.klv")?);
@@ -21,14 +81,18 @@ pub fn main() -> error::Returns<()> {
     let mut move_picker_1 =
         move_picker::MovePicker::Simmer(move_picker::Simmer::new(game_config, &kwg, &klv));
 
+    let mut game_state = game_state::GameState::new(game_config);
+    let mut rng = rand_chacha::ChaCha20Rng::from_entropy();
+    let mut timers = GameTimers::new(game_config.num_players());
     loop {
-        let mut game_state = game_state::GameState::new(game_config);
-        let mut rng = rand_chacha::ChaCha20Rng::from_entropy();
         game_state.reset_and_draw_tiles(&mut rng);
         let mut final_scores = vec![0; game_state.players.len()];
+        timers.reset_to(25 * 60 * 1000);
 
         loop {
             display::print_game_state(&game_state);
+            timers.set_turn(game_state.turn as i8);
+            print_timers(&timers);
 
             let filtered_movegen = if game_state.turn == 0 {
                 &mut filtered_movegen_0
@@ -85,9 +149,23 @@ pub fn main() -> error::Returns<()> {
             }
             game_state.next_turn();
         }
+        timers.set_turn(-1);
 
         display::print_game_state(&game_state);
         println!("Final scores: {:?}", final_scores);
+        print_timers(&timers);
+        let mut has_time_penalty = false;
+        for (i, &timer) in timers.clocks_ms.iter().enumerate() {
+            if timer < 0 {
+                let penalty = (((!timer / 60000) + 1) * 10) as i16;
+                println!("Player {} penalty {}", i + 1, penalty);
+                final_scores[i] -= penalty;
+                has_time_penalty = true;
+            }
+        }
+        if has_time_penalty {
+            println!("Really final scores: {:?}", final_scores);
+        }
     } // temp loop
 
     //Ok(())
