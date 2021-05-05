86dd841ae338faa9511869efd68d185ca53f436e
Andy Kurnia <andykurn@gmail.com>
2021-05-06 01:15:00 +0800

    add useful script

---
+++ build-liwords-kwg.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# ./build-liwords-kwg.sh english something.txt CSW19X
+
+set -euo pipefail
+
+lexlang="$1"
+infile="$2"
+lexname="$3"
+
+liwordsdir="../liwords"
+dawgpath="${liwordsdir}/data/lexica/dawg/${lexname}.dawg"
+gaddagpath="${liwordsdir}/liwords-ui/public/wasm/${lexname}.gaddag"
+kwgpath="${liwordsdir}/liwords-ui/public/wasm/$(echo "$lexname" | tr A-Z a-z).kwg"
+
+cargo run --release --bin buildlex -- "${lexlang}-kwg" "$infile" "$kwgpath"
+cargo run --release --bin buildlex -- "${lexlang}-macondo" "$kwgpath" "$lexname" "$dawgpath" "$gaddagpath"
