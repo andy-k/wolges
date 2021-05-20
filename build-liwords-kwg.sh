442d30177190ddf5127702ec4623682f96cb08d9
Andy Kurnia <andykurn@gmail.com>
2021-05-20 13:54:35 +0800

    use caps

--- build-liwords-kwg.sh
+++ build-liwords-kwg.sh
@@ -10,7 +10,7 @@ lexname="$3"
 liwordsdir="../liwords"
 dawgpath="${liwordsdir}/data/lexica/dawg/${lexname}.dawg"
 gaddagpath="${liwordsdir}/liwords-ui/public/wasm/${lexname}.gaddag"
-kwgpath="${liwordsdir}/liwords-ui/public/wasm/$(echo "$lexname" | tr A-Z a-z).kwg"
+kwgpath="${liwordsdir}/liwords-ui/public/wasm/${lexname}.kwg"
 
 cargo run --release --bin buildlex -- "${lexlang}-kwg" "$infile" "$kwgpath"
 cargo run --release --bin buildlex -- "${lexlang}-macondo" "$kwgpath" "$lexname" "$dawgpath" "$gaddagpath"
