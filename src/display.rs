65e4ecadd10977b58a4643a5760cb7387d340122
Andy Kurnia <andykurn@gmail.com>
2021-12-10 11:52:17 +0800

    ignore case column parsing

--- src/display.rs
+++ src/display.rs
@@ -79,6 +79,27 @@ pub fn str_to_column_usize(sb: &[u8]) -> Option<usize> {
     }
 }
 
+// Parses ColumnStr strings (passed as str.as_bytes()).
+pub fn str_to_column_usize_ignore_case(sb: &[u8]) -> Option<usize> {
+    if sb.is_empty() {
+        return None;
+    }
+    let c = sb[0] & !0x20;
+    if (0x41..=0x5a).contains(&c) {
+        let mut v = c as usize - 0x41;
+        for &c in sb[1..].iter() {
+            if (0x41..=0x5a).contains(&c) {
+                v = v.checked_mul(26)?.checked_add(c as usize - (0x41 - 26))?;
+            } else {
+                return None;
+            }
+        }
+        Some(v)
+    } else {
+        None
+    }
+}
+
 pub fn print_board(
     alphabet: &alphabet::Alphabet<'_>,
     board_layout: &board_layout::BoardLayout,
