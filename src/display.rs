cc40a66aa2ae2743f9550fd3aaafdfb9d7aca59b
Andy Kurnia <andykurn@gmail.com>
2022-04-17 15:04:40 +0800

    consolidate print in display

--- src/display.rs
+++ src/display.rs
@@ -100,53 +100,134 @@ pub fn str_to_column_usize_ignore_case(sb: &[u8]) -> Option<usize> {
     }
 }
 
+struct BoardPrinter<'a> {
+    alphabet: &'a alphabet::Alphabet<'a>,
+    board_layout: &'a board_layout::BoardLayout,
+    board_tiles: &'a [u8],
+}
+
+impl std::fmt::Display for BoardPrinter<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "  ")?;
+        for c in 0..self.board_layout.dim().cols {
+            write!(f, " {}", column(c))?;
+        }
+        writeln!(f)?;
+        write!(f, "  +")?;
+        for _ in 1..self.board_layout.dim().cols {
+            write!(f, "--")?;
+        }
+        writeln!(f, "-+")?;
+        for r in 0..self.board_layout.dim().rows {
+            write!(f, "{:2}|", r + 1)?;
+            for c in 0..self.board_layout.dim().cols {
+                if c > 0 {
+                    write!(f, " ")?
+                }
+                write!(
+                    f,
+                    "{}",
+                    board_label(self.alphabet, self.board_layout, self.board_tiles, r, c)
+                )?;
+            }
+            writeln!(f, "|{}", r + 1)?;
+        }
+        write!(f, "  +")?;
+        for _ in 1..self.board_layout.dim().cols {
+            write!(f, "--")?;
+        }
+        writeln!(f, "-+")?;
+        write!(f, "  ")?;
+        for c in 0..self.board_layout.dim().cols {
+            write!(f, " {}", column(c))?;
+        }
+        writeln!(f)?;
+        Ok(())
+    }
+}
+
 pub fn print_board(
     alphabet: &alphabet::Alphabet<'_>,
     board_layout: &board_layout::BoardLayout,
     board_tiles: &[u8],
 ) {
-    print!("  ");
-    for c in 0..board_layout.dim().cols {
-        print!(" {}", column(c));
-    }
-    println!();
-    print!("  +");
-    for _ in 1..board_layout.dim().cols {
-        print!("--");
-    }
-    println!("-+");
-    for r in 0..board_layout.dim().rows {
-        print!("{:2}|", r + 1);
-        for c in 0..board_layout.dim().cols {
-            if c > 0 {
-                print!(" ")
-            }
-            print!("{}", board_label(alphabet, board_layout, board_tiles, r, c));
+    print!(
+        "{}",
+        BoardPrinter {
+            alphabet,
+            board_layout,
+            board_tiles
         }
-        println!("|{}", r + 1);
-    }
-    print!("  +");
-    for _ in 1..board_layout.dim().cols {
-        print!("--");
-    }
-    println!("-+");
-    print!("  ");
-    for c in 0..board_layout.dim().cols {
-        print!(" {}", column(c));
-    }
-    println!();
+    );
 }
 
-fn print_ms(mut ms: i64) {
-    if ms < 0 {
-        print!("-");
-        ms = -ms;
+struct MsPrinter {
+    ms: i64,
+}
+
+impl std::fmt::Display for MsPrinter {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut ms = self.ms;
+        if ms < 0 {
+            write!(f, "-")?;
+            ms = -ms;
+        }
+        let just_ms = ms % 1000;
+        let sec = ms / 1000;
+        let just_sec = sec % 60;
+        let min = sec / 60;
+        write!(f, "{:02}:{:02}.{:03}", min, just_sec, just_ms)?;
+        Ok(())
     }
-    let just_ms = ms % 1000;
-    let sec = ms / 1000;
-    let just_sec = sec % 60;
-    let min = sec / 60;
-    print!("{:02}:{:02}.{:03}", min, just_sec, just_ms);
+}
+
+struct GameStatePrinter<'a> {
+    game_config: &'a game_config::GameConfig<'a>,
+    game_state: &'a game_state::GameState,
+    optional_game_timers: Option<&'a game_timers::GameTimers>,
+}
+
+impl std::fmt::Display for GameStatePrinter<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(
+            f,
+            "{}Pool {}: {}",
+            BoardPrinter {
+                alphabet: self.game_config.alphabet(),
+                board_layout: self.game_config.board_layout(),
+                board_tiles: &self.game_state.board_tiles,
+            },
+            self.game_state.bag.0.len(),
+            self.game_config.alphabet().fmt_rack(&self.game_state.bag.0)
+        )?;
+        let now = std::time::Instant::now();
+        for (i, player) in self.game_state.players.iter().enumerate() {
+            write!(
+                f,
+                "Player {}: {} {}",
+                i + 1,
+                player.score,
+                self.game_config.alphabet().fmt_rack(&player.rack)
+            )?;
+            if let Some(game_timers) = self.optional_game_timers {
+                let clock_ms = game_timers.get_timer_as_at(now, i);
+                write!(f, " {}", MsPrinter { ms: clock_ms })?;
+                let adjustment = self.game_config.time_adjustment(clock_ms);
+                if adjustment != 0 {
+                    write!(f, " ({})", adjustment)?;
+                }
+                if game_timers.turn as usize == i {
+                    // may differ from game_state.turn if timer is paused
+                    write!(f, " (timer running)")?;
+                }
+            }
+            if self.game_state.turn as usize == i {
+                write!(f, " (turn)")?;
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
 }
 
 pub fn print_game_state(
@@ -154,40 +235,12 @@ pub fn print_game_state(
     game_state: &game_state::GameState,
     optional_game_timers: Option<&game_timers::GameTimers>,
 ) {
-    print_board(
-        game_config.alphabet(),
-        game_config.board_layout(),
-        &game_state.board_tiles,
-    );
-    println!(
-        "Pool {}: {}",
-        game_state.bag.0.len(),
-        game_config.alphabet().fmt_rack(&game_state.bag.0)
-    );
-    let now = std::time::Instant::now();
-    for (i, player) in game_state.players.iter().enumerate() {
-        print!(
-            "Player {}: {} {}",
-            i + 1,
-            player.score,
-            game_config.alphabet().fmt_rack(&player.rack)
-        );
-        if let Some(game_timers) = optional_game_timers {
-            let clock_ms = game_timers.get_timer_as_at(now, i);
-            print!(" ");
-            print_ms(clock_ms);
-            let adjustment = game_config.time_adjustment(clock_ms);
-            if adjustment != 0 {
-                print!(" ({})", adjustment);
-            }
-            if game_timers.turn as usize == i {
-                // may differ from game_state.turn if timer is paused
-                print!(" (timer running)");
-            }
+    print!(
+        "{}",
+        GameStatePrinter {
+            game_config,
+            game_state,
+            optional_game_timers
         }
-        if game_state.turn as usize == i {
-            print!(" (turn)");
-        }
-        println!();
-    }
+    );
 }
