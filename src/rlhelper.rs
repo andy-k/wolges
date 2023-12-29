d37948864c140b9c44ee1c0888866984e473c834
Andy Kurnia <andykurn@gmail.com>
2023-12-30 04:12:05 +0800

    upgrade rustyline

--- src/rlhelper.rs
+++ src/rlhelper.rs
@@ -53,8 +53,8 @@ impl rustyline::highlight::Highlighter for MyHelper {
         self.highlighter.highlight(line, pos)
     }
 
-    fn highlight_char(&self, line: &str, pos: usize) -> bool {
-        self.highlighter.highlight_char(line, pos)
+    fn highlight_char(&self, line: &str, pos: usize, forced: bool) -> bool {
+        self.highlighter.highlight_char(line, pos, forced)
     }
 }
 
